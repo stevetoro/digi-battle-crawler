cbb0e4a9984911917d8fb1056aeb4bb5d19b9887
Steve Toro <steve.toro@gmail.com>
2024-03-02 11:26:09 -0700

    Fixes power option effect parsing.

--- lib/power_option_parser.ex
+++ lib/power_option_parser.ex
@@ -4,7 +4,7 @@ defmodule DigiBattleCrawler.PowerOptionParser do
     card_set = document |> Floki.find("h5:fl-contains('Set:') a") |> Floki.text()
     card_number = document |> Floki.find(".serial-code") |> Floki.text()
     type = document |> Floki.find("h5:fl-contains('Type:') span") |> Floki.text()
-    effect = document |> Floki.find(".EnglishPOEffect") |> Floki.text()
+    effect = document |> Floki.find("#EnglishPOEffect") |> Floki.text()
 
     restrictions =
       document |> Floki.find("#EnglishRestrictions li") |> Enum.map(fn x -> Floki.text(x) end)
