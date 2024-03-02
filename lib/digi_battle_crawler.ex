c5590047f2468e972a23e4964eba7e93783b896d
Steve Toro <steve.toro@gmail.com>
2024-03-02 10:54:59 -0700

    Adds digimon card detail parser.

--- lib/digi_battle_crawler.ex
+++ lib/digi_battle_crawler.ex
@@ -1,5 +1,6 @@
 defmodule DigiBattleCrawler do
   use Crawly.Spider
+  alias DigiBattleCrawler.DigimonParser, as: DigimonParser
 
   @impl Crawly.Spider
   def base_url, do: "https://digi-battle.com"
@@ -19,6 +20,15 @@ defmodule DigiBattleCrawler do
   def parse_item(response) do
     {:ok, document} = Floki.parse_document(response.body)
 
+    items =
+      cond do
+        card_set_page?(response.request_url) ->
+          []
+
+        card_detail_page?(response.request_url) ->
+          [DigimonParser.parse(document)]
+      end
+
     requests =
       document
       |> Floki.find(".gallery a")
@@ -29,8 +39,16 @@ defmodule DigiBattleCrawler do
       end)
 
     %Crawly.ParsedItem{
-      :items => [],
+      :items => items,
       :requests => requests
     }
+  end
+
+  defp card_set_page?(request_url) do
+    request_url |> String.contains?("/Sets/")
+  end
+
+  defp card_detail_page?(request_url) do
+    request_url |> String.contains?("/Card/Details/")
   end
 end
