a4f874931b4b630fb1a13c6199f776b82a8e8134
Steve Toro <steve.toro@gmail.com>
2024-03-01 19:13:49 -0700

    Implements basic crawling.

--- lib/digi_battle_crawler.ex
+++ lib/digi_battle_crawler.ex
@@ -1,2 +1,36 @@
 defmodule DigiBattleCrawler do
+  use Crawly.Spider
+
+  @impl Crawly.Spider
+  def base_url, do: "https://digi-battle.com"
+
+  @impl Crawly.Spider
+  def init do
+    [
+      start_urls: [
+        "https://digi-battle.com/Sets/StarterSet",
+        "https://digi-battle.com/Sets/BoosterSet1",
+        "https://digi-battle.com/Sets/BoosterSet2"
+      ]
+    ]
+  end
+
+  @impl Crawly.Spider
+  def parse_item(response) do
+    {:ok, document} = Floki.parse_document(response.body)
+
+    requests =
+      document
+      |> Floki.find(".gallery a")
+      |> Floki.attribute("href")
+      |> Enum.map(fn url ->
+        Crawly.Utils.build_absolute_url(url, response.request_url)
+        |> Crawly.Utils.request_from_url()
+      end)
+
+    %Crawly.ParsedItem{
+      :items => [],
+      :requests => requests
+    }
+  end
 end
