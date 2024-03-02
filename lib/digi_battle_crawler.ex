fb411254084c27deb65e0303de6b5a99f2ef00b2
Steve Toro <steve.toro@gmail.com>
2024-03-01 19:01:27 -0700

    Initial commit.

---
+++ lib/digi_battle_crawler.ex
@@ -0,0 +1,18 @@
+defmodule DigiBattleCrawler do
+  @moduledoc """
+  Documentation for `DigiBattleCrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DigiBattleCrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/digi_battle_crawler_test.exs
@@ -0,0 +1,8 @@
+defmodule DigiBattleCrawlerTest do
+  use ExUnit.Case
+  doctest DigiBattleCrawler
+
+  test "greets the world" do
+    assert DigiBattleCrawler.hello() == :world
+  end
+end
