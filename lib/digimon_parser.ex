52c648d53209229da563d176bd1f47b25c2f3720
Steve Toro <steve.toro@gmail.com>
2024-03-03 16:47:22 -0700

    Refactors digimon parser to make better use of structs and pattern matching.

--- lib/digimon_parser.ex
+++ lib/digimon_parser.ex
@@ -1,23 +1,85 @@
 defmodule DigiBattleCrawler.DigimonParser do
+  alias DigiBattleCrawler.DigimonParser
   alias DigiBattleCrawler.ImageDownloader
 
+  defstruct document: nil,
+            sidepane: nil,
+            wide_table: nil,
+            digimon: %{
+              name: nil,
+              card_number: nil,
+              group: nil,
+              level: nil,
+              battle_type: nil,
+              attacks: %{red: nil, green: nil, yellow: nil},
+              card_set: nil,
+              card_type: nil,
+              special_effect: nil,
+              digimon_type: nil,
+              special_ability: nil,
+              scores: %{
+                rookie: nil,
+                champion: nil,
+                ultimate: nil,
+                mega: nil
+              }
+            },
+            card_image_url: nil
+
   def parse(document) do
-    sidepane = parse_side_pane(document)
-    wide_table = parse_wide_table(document)
-    Map.merge(sidepane, wide_table)
+    %DigimonParser{document: document}
+    |> find_sidepane()
+    |> find_wide_table()
+    |> parse_digimon_name()
+    |> parse_card_number()
+    |> parse_digimon_group()
+    |> parse_digimon_level()
+    |> parse_digimon_battle_type()
+    |> parse_digimon_attacks()
+    |> parse_wide_table_attributes()
+    |> parse_card_image_url()
+    |> download_card_scan()
+    |> then(fn p -> p.digimon end)
   end
 
-  defp parse_side_pane(document) do
+  defp find_sidepane(%DigimonParser{document: document} = parser) do
     sidepane =
       document
       |> Floki.find(".card-details-sidepane")
 
-    name = sidepane |> Floki.find("#EnglishName") |> Floki.text()
-    card_number = sidepane |> Floki.find(".serial-code") |> Floki.text()
-    card_image_url = document |> Floki.find("#CardSmall") |> Floki.attribute("src") |> Enum.at(0)
-    group = sidepane |> Floki.find("#EnglishDescription") |> Floki.text()
-    level = sidepane |> Floki.find("#EnglishDigimonLevel") |> Floki.text()
+    %DigimonParser{parser | sidepane: sidepane}
+  end
 
+  defp find_wide_table(%DigimonParser{document: document} = parser) do
+    wide_table =
+      document
+      |> Floki.find(".wide-table tr>td:nth-child(2)")
+      |> Enum.map(fn x -> Floki.text(x) end)
+
+    %DigimonParser{parser | wide_table: wide_table}
+  end
+
+  defp parse_digimon_name(%DigimonParser{sidepane: sidepane, digimon: digimon} = parser) do
+    name = sidepane |> Floki.find("#EnglishName") |> Floki.text()
+    %DigimonParser{parser | digimon: %{digimon | name: name}}
+  end
+
+  defp parse_card_number(%DigimonParser{sidepane: sidepane, digimon: digimon} = parser) do
+    card_number = sidepane |> Floki.find(".serial-code") |> Floki.text()
+    %DigimonParser{parser | digimon: %{digimon | card_number: card_number}}
+  end
+
+  defp parse_digimon_group(%DigimonParser{sidepane: sidepane, digimon: digimon} = parser) do
+    group = sidepane |> Floki.find("#EnglishDescription") |> Floki.text()
+    %DigimonParser{parser | digimon: %{digimon | group: group}}
+  end
+
+  defp parse_digimon_level(%DigimonParser{sidepane: sidepane, digimon: digimon} = parser) do
+    level = sidepane |> Floki.find("#EnglishDigimonLevel") |> Floki.text()
+    %DigimonParser{parser | digimon: %{digimon | level: level}}
+  end
+
+  defp parse_digimon_battle_type(%DigimonParser{sidepane: sidepane, digimon: digimon} = parser) do
     battle_type =
       sidepane
       |> Floki.find(".battle-type")
@@ -26,22 +88,23 @@ defmodule DigiBattleCrawler.DigimonParser do
       |> Enum.at(0)
       |> String.replace("battle-type ", "")
 
+    %DigimonParser{parser | digimon: %{digimon | battle_type: battle_type}}
+  end
+
+  defp parse_digimon_attacks(%DigimonParser{sidepane: sidepane, digimon: digimon} = parser) do
     [red, green, yellow] =
       sidepane |> Floki.find(".row.w-100 > .col-12 div")
 
-    ImageDownloader.download(card_image_url, card_number)
-
-    %{
-      name: name,
-      card_number: card_number,
-      group: group,
-      level: level,
-      battle_type: battle_type,
-      attacks: %{
-        red: parse_attack(red),
-        green: parse_attack(green),
-        yellow: parse_attack(yellow)
-      }
+    %DigimonParser{
+      parser
+      | digimon: %{
+          digimon
+          | attacks: %{
+              red: parse_attack(red),
+              green: parse_attack(green),
+              yellow: parse_attack(yellow)
+            }
+        }
     }
   end
 
@@ -51,24 +114,38 @@ defmodule DigiBattleCrawler.DigimonParser do
     %{name: name, power: power}
   end
 
-  defp parse_wide_table(document) do
-    wide_table =
-      document
-      |> Floki.find(".wide-table tr>td:nth-child(2)")
-      |> Enum.map(fn x -> Floki.text(x) end)
-
-    %{
-      card_set: Enum.at(wide_table, 0),
-      card_type: Enum.at(wide_table, 1),
-      special_effect: Enum.at(wide_table, 2),
-      digimon_type: Enum.at(wide_table, 3),
-      special_ability: Enum.at(wide_table, 4),
-      scores: %{
-        rookie: Enum.at(wide_table, 5),
-        champion: Enum.at(wide_table, 6),
-        ultimate: Enum.at(wide_table, 7),
-        mega: Enum.at(wide_table, 8)
-      }
+  defp parse_wide_table_attributes(
+         %DigimonParser{wide_table: wide_table, digimon: digimon} = parser
+       ) do
+    %DigimonParser{
+      parser
+      | digimon: %{
+          digimon
+          | card_set: Enum.at(wide_table, 0),
+            card_type: Enum.at(wide_table, 1),
+            special_effect: Enum.at(wide_table, 2),
+            digimon_type: Enum.at(wide_table, 3),
+            special_ability: Enum.at(wide_table, 4),
+            scores: %{
+              rookie: Enum.at(wide_table, 5),
+              champion: Enum.at(wide_table, 6),
+              ultimate: Enum.at(wide_table, 7),
+              mega: Enum.at(wide_table, 8)
+            }
+        }
     }
+  end
+
+  defp parse_card_image_url(%DigimonParser{document: document} = parser) do
+    card_image_url = document |> Floki.find("#CardSmall") |> Floki.attribute("src") |> Enum.at(0)
+    %DigimonParser{parser | card_image_url: card_image_url}
+  end
+
+  defp download_card_scan(
+         %DigimonParser{card_image_url: card_image_url, digimon: %{card_number: card_number}} =
+           parser
+       ) do
+    ImageDownloader.download(card_image_url, card_number)
+    parser
   end
 end
